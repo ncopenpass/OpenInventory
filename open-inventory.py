053222839303b6acd60da2c0526d4e99859b2658
mbharadwaj <mbharadwaj@gmail.com>
2018-08-15 18:12:53 -0400

    removed unused imports

--- open-inventory.py
+++ open-inventory.py
@@ -1,9 +1,8 @@
 from app import create_app, db
-from app.models import Restaurant, Menu, MenuItem
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Restaurant': Restaurant, 'Menu': Menu, 'MenuItem': MenuItem}
+    return {'db': db}
 
